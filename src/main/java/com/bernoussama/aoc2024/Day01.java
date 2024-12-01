0b0f50d1f8188b53d2034d3d1b85c28488961b80
bernoussama <oussama.bernou@outlook.com>
2024-12-01 15:43:16 +0100

    day 1

---
+++ src/main/java/com/bernoussama/aoc2024/Day01.java
@@ -0,0 +1,57 @@
+package com.bernoussama.aoc2024;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JWindow;
+
+public class Day01 implements Day {
+
+  private ArrayList<Integer> list1 = new ArrayList<Integer>();
+  private ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+  public Day01(Path path) {
+    try {
+      Files.lines(path).forEach((n) -> {
+        var line = n.split("   ");
+        list1.add(Integer.parseInt(line[0]));
+        list2.add(Integer.parseInt(line[1]));
+      });
+    } catch (IOException e) {
+      System.err.println("error reading file");
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public String part1() {
+    int totalDistance = 0;
+    list1.sort(Comparator.naturalOrder());
+    list2.sort(Comparator.naturalOrder());
+
+    for (int i = 0; i < list1.size(); i++) {
+      totalDistance += Math.abs(list1.get(i) - list2.get(i));
+    }
+    return Integer.toString(totalDistance);
+  }
+
+  @Override
+  public String part2() {
+    int similarity = 0;
+    for (int lNum : list1) {
+      int occurences = 0;
+      for (int rNum : list2) {
+        if (lNum == rNum) {
+          occurences++;
+        }
+      }
+      similarity += lNum * occurences;
+    }
+    return Integer.toString(similarity);
+  }
+}
